43739216006f9b9cbd257f9f83c52de30d057371
Nadav Dayan <ndayan@appdev.yearup.org>
2023-04-12 10:02:22 -0400

    Added a new Java class

---
+++ MyCodingSkills.java
@@ -0,0 +1,11 @@
+public class MyCodingSkills {
+    public static void main (String[] args){
+        System.out.println("Java");
+        System.out.println("SQL");
+        System.out.println("HTML");
+        System.out.println("Google Sheets");
+        System.out.println("Windows Active Directory");
+        System.out.println("CDK");
+        System.out.println("Reynolds and Reynolds");
+    }
+}
